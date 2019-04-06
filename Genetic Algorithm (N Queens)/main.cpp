25202f1a59cd4d03b46c20d262d650f58d2ee4bb
uonliaquat <antelopes06@gmail.com>
2019-04-07 01:40:14 +0500

    Initial Commit

---
+++ Genetic Algorithm (N Queens)/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Genetic Algorithm (N Queens)
+//
+//  Created by Uon Liaquat on 07/04/2019.
+//  Copyright © 2019 Uon Liaquat. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
